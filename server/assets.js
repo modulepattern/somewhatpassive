d125b9b7f00d22614d6be22abc95bf36ebac4de1
Sri <srirangan@gmail.com>
2017-11-02 15:02:45 +0100

    fixup

--- server/assets.js
+++ server/assets.js
@@ -21,18 +21,13 @@ function serve(domain, req, res) {
     const domain_file_path = get_file_path(domain, req);
     const common_file_path = get_common_file_path(req);
     const file_path = fs.existsSync(domain_file_path) ? domain_file_path : common_file_path;
-    const content_type = mime.lookup(file_path);
-    const charset = mime.charsets.lookup(content_type);
-    const extension = mime.extension(content_type);
-    const file_contents = fs.readFileSync(file_path, charset);
+    const content_type = mime.getType(file_path);
+    const extension = mime.getExtension(content_type);
+    const file_contents = fs.readFileSync(file_path);
     const text_extensions = ['html', 'css', 'js', 'svg', 'md', 'txt', 'xml'];
     const is_text = text_extensions.indexOf(extension) > -1;
-    if (is_text) {
-        res.end(file_contents);
-    }
-    else {
-        res.end(file_contents, 'binary');
-    }
+    if (is_text) res.end(file_contents);
+    else res.end(file_contents, 'binary');
 }
 
 module.exports.is = is;
