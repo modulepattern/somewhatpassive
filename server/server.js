cd9804142a477eb15270c51451d8851721dc0fd4
Sri <srirangan@gmail.com>
2018-03-19 16:39:32 +0100

    remove posts module

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 const http = require('http');
-
 const domains = require('./domains');
 const assets = require('./assets');
 const pages = require('./pages');
-const posts = require('./posts');
 const errors = require('./errors');
 
 const server = http.createServer();
@@ -13,7 +11,6 @@ server.on('request', function (req, res) {
     if (domain) {
         if (assets.is(domain, req)) assets.serve(domain, req, res);
         else if (pages.is(domain, req)) pages.serve(domain, req, res);
-        else if (posts.is(domain, req)) posts.serve(domain, req, res);
         else errors.serve(404, domain, req, res);
     }
     res.end();
